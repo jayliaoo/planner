4cb7b3dc305b74c401f5083c4c7657cd9fc932fd
Jay Liao <liao.jia@shipper.id>
2023-06-15 23:03:56 +0800

    update

--- planner-be/src/main.rs
+++ planner-be/src/main.rs
@@ -1,3 +1,4 @@
+use std::panic::catch_unwind;
 use std::sync::Arc;
 
 use axum::http::{HeaderValue, Method};
@@ -23,74 +24,80 @@ mod service;
 
 #[tokio::main]
 async fn main() {
-    let pool = get_connection_pool();
-    let user_repository = Arc::new(UserRepository::new(pool.clone()));
-    let sprint_repository = Arc::new(SprintRepository::new(pool.clone()));
-    let task_service = Arc::new(TaskService::new(
-        Arc::new(TaskRepository::new(pool)),
-        user_repository.clone(),
-        sprint_repository.clone(),
-    ));
+    let result = catch_unwind(|| async {
+        let pool = get_connection_pool();
+        let user_repository = Arc::new(UserRepository::new(pool.clone()));
+        let sprint_repository = Arc::new(SprintRepository::new(pool.clone()));
+        let task_service = Arc::new(TaskService::new(
+            Arc::new(TaskRepository::new(pool)),
+            user_repository.clone(),
+            sprint_repository.clone(),
+        ));
 
-    let base_jwt = BaseJwt::new("6d4f5439-e33a-45e1-b914-9bbe18ee7137".to_string());
-    let jwt_arc = Arc::new(base_jwt.clone());
-    let jwt = Jwt::new(base_jwt);
-    let cors_layer = CorsLayer::new()
-        .allow_headers(AllowHeaders::mirror_request())
-        .allow_methods(AllowMethods::list([
-            Method::POST,
-            Method::GET,
-            Method::PUT,
-            Method::DELETE,
-        ]))
-        .allow_credentials(true)
-        .allow_origin(AllowOrigin::list([HeaderValue::from_str(
-            "http://localhost:4200",
-        )
-        .unwrap()]));
+        let base_jwt = BaseJwt::new("6d4f5439-e33a-45e1-b914-9bbe18ee7137".to_string());
+        let jwt_arc = Arc::new(base_jwt.clone());
+        let jwt = Jwt::new(base_jwt);
+        let cors_layer = CorsLayer::new()
+            .allow_headers(AllowHeaders::mirror_request())
+            .allow_methods(AllowMethods::list([
+                Method::POST,
+                Method::GET,
+                Method::PUT,
+                Method::DELETE,
+            ]))
+            .allow_credentials(true)
+            .allow_origin(AllowOrigin::list([HeaderValue::from_str(
+                "http://localhost:4200",
+            )
+            .unwrap()]));
 
-    let app = Router::new()
-        .route("/login", post(user_handler::login).with_state(jwt_arc))
-        .route("/index", get(|| async { Html("Hello World!") }))
-        .nest(
-            "",
-            Router::new()
-                .nest(
-                    "/user",
-                    Router::new()
-                        .route("/", post(user_handler::add))
-                        .route("/", put(user_handler::edit))
-                        .route("/:id", delete(user_handler::delete))
-                        .route("/:id", get(user_handler::get))
-                        .route("/list", get(user_handler::get_list))
-                        .with_state(user_repository),
-                )
-                .nest(
-                    "/sprint",
-                    Router::new()
-                        .route("/", post(sprint_handler::add))
-                        .route("/", put(sprint_handler::edit))
-                        .route("/:id", delete(sprint_handler::delete))
-                        .route("/:id", get(sprint_handler::get))
-                        .route("/list", get(sprint_handler::get_all))
-                        .with_state(sprint_repository),
-                )
-                .nest(
-                    "/task",
-                    Router::new()
-                        .route("/", post(task_handler::add))
-                        .route("/", put(task_handler::edit))
-                        .route("/:id", delete(task_handler::delete))
-                        .route("/:id", get(task_handler::get))
-                        .route("/list", get(task_handler::get_list))
-                        .with_state(task_service),
-                )
-                .layer(ValidateRequestHeaderLayer::custom(jwt)),
-        )
-        .layer(cors_layer);
+        let app = Router::new()
+            .route("/login", post(user_handler::login).with_state(jwt_arc))
+            .route("/index", get(|| async { Html("Hello World!") }))
+            .nest(
+                "",
+                Router::new()
+                    .nest(
+                        "/user",
+                        Router::new()
+                            .route("/", post(user_handler::add))
+                            .route("/", put(user_handler::edit))
+                            .route("/:id", delete(user_handler::delete))
+                            .route("/:id", get(user_handler::get))
+                            .route("/list", get(user_handler::get_list))
+                            .with_state(user_repository),
+                    )
+                    .nest(
+                        "/sprint",
+                        Router::new()
+                            .route("/", post(sprint_handler::add))
+                            .route("/", put(sprint_handler::edit))
+                            .route("/:id", delete(sprint_handler::delete))
+                            .route("/:id", get(sprint_handler::get))
+                            .route("/list", get(sprint_handler::get_all))
+                            .with_state(sprint_repository),
+                    )
+                    .nest(
+                        "/task",
+                        Router::new()
+                            .route("/", post(task_handler::add))
+                            .route("/", put(task_handler::edit))
+                            .route("/:id", delete(task_handler::delete))
+                            .route("/:id", get(task_handler::get))
+                            .route("/list", get(task_handler::get_list))
+                            .with_state(task_service),
+                    )
+                    .layer(ValidateRequestHeaderLayer::custom(jwt)),
+            )
+            .layer(cors_layer);
 
-    axum::Server::bind(&"0.0.0.0:3500".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        axum::Server::bind(&"0.0.0.0:3500".parse().unwrap())
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    });
+    match result {
+        Ok(_) => println!("OK"),
+        Err(e) => println!("{:?}", e),
+    }
 }
