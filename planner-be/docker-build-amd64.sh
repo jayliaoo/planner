16dc63da8208e374f5efb5373523e364582c4757
Jay Liao <liao.jia@shipper.id>
2023-06-16 14:42:54 +0800

    update

--- planner-be/docker-build-amd64.sh
+++
@@ -1,4 +0,0 @@
-export tag=`git rev-parse --short HEAD`
-docker build -t planner-be-amd64:$tag -f Dockerfile.amd64 .
-docker tag planner-be-amd64:$tag jayliaoo.azurecr.io/planner-be:$tag
-docker push jayliaoo.azurecr.io/planner-be:$tag
